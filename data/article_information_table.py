6aa7461d5a46ab259c18c8b1b12c6821f0359fbb
andrew-lockwood <andrewlockwood579@gmail.com>
2017-02-04 14:59:45 -0500

    Added Multiprocessing

--- data/article_information_table.py
+++ data/article_information_table.py
@@ -1,74 +1,117 @@
-
+# Database
 import sqlite3
-from context import settings, ProgressBar
 
-
-from bs4 import BeautifulSoup
-import urllib.request
-import multiprocessing
+# Time
 import datetime
+import time
 
+# Text 
+from bs4 import BeautifulSoup
 import re
+
+# System 
+from multiprocessing import Pool, Manager
+import sys
 import os
-import operator
-from nltk.stem.porter import *
+
+# Project 
+from context import settings
+
 
 def ArticleInformation():
-    data = []
-    files = os.listdir(settings.xml)
-    pb = ProgressBar(len(files))
+    q = """CREATE TABLE ArticleInformation(
+                articleID   TEXT PRIMARY KEY,
+                title       TEXT,
+                received    DATE, 
+                type        TEXT)"""
 
-    for file in files:
-        pb.step()
-        with open(os.path.join(settings.xml, file)) as xml:
-            soup = BeautifulSoup(xml, 'lxml')
-            try:
-                articleID = re.sub('10.3389/', '',
-                                   soup.find('article-id').get_text())
-
-                # Can be either accepted or received
-                #d = soup.find(attrs={"date-type":"accepted"})
-                d = soup.find(attrs={"date-type": "received"})
-                day = d.find('day').get_text()
-                month = d.find('month').get_text()
-                year = d.find('year').get_text()
-
-                # Standardizes the date format to "YYYY-MM-DD"
-                # Confirms days/months have a leading zero
-                if len(day) == 1:
-                    day = '0' + day
-                if len(month) == 1:
-                    month = '0' + month
-                if len(year) == 2:
-                    year = '20' + year
-
-                date = '%s-%s-%s' % (year, month, day)
-
-                article_type = re.sub('-', ' ', soup.find('article')
-                                      .get('article-type'))
-
-                # Removes all non-ascii characters/spaces greater
-                # than 1 from the title
-                title = ''.join([i if ord(i) < 128 else ' ' for i in
-                                 soup.find('article-title').get_text()])
-                title = re.sub(' +', ' ', title)
-
-                # Assembles the tuple
-                data.append((articleID, date, article_type, title))
-
-            except AttributeError:
-                continue
-            except sqlite3.IntegrityError:
-                continue
-
-    for row in data: 
-        curr.execute("""INSERT INTO ArticleInformation 
-                        VALUES (?, ?, ?, ?)""", row)
-
+    curr.execute(q)
     conn.commit()
+
+
+def process_file(args):
+    """Returns """
+    file, q = args
+    with open(os.path.join(settings.xml, file)) as xml:
+        soup = BeautifulSoup(xml, 'lxml')
+        try:
+            articleID = re.sub('10.3389/', '',
+                               soup.find('article-id').get_text())
+
+            # Can be either accepted or received
+            #d = soup.find(attrs={"date-type":"accepted"})
+            d = soup.find(attrs={"date-type": "received"})
+            day = d.find('day').get_text()
+            month = d.find('month').get_text()
+            year = d.find('year').get_text()
+
+            # Standardizes the date format to "YYYY-MM-DD"
+            # Confirms days/months have a leading zero
+            if len(day) == 1:
+                day = '0' + day
+            if len(month) == 1:
+                month = '0' + month
+            if len(year) == 2:
+                year = '20' + year
+
+            date = '%s-%s-%s' % (year, month, day)
+
+            article_type = re.sub('-', ' ', soup.find('article')
+                                  .get('article-type'))
+
+            # Removes all non-ascii characters/spaces greater
+            # than 1 from the title
+            title = ''.join([i if ord(i) < 128 else ' ' for i in
+                             soup.find('article-title').get_text()])
+            title = re.sub(' +', ' ', title)
+
+            # Assembles the tuple
+            t = (articleID, title, date, article_type)
+            q.put(t)
+            return t
+
+        except AttributeError:
+            return
+        except sqlite3.IntegrityError:
+            return 
 
 
 if __name__ == "__main__":
     conn = sqlite3.connect(settings.db)
     curr = conn.cursor()
-    ArticleInformation()
+
+    #ArticleInformation()
+
+    multi = True
+
+    if multi: 
+        p = Pool(4)
+        m = Manager()
+        q = m.Queue()
+
+        files = [(f, q) for f in os.listdir(settings.xml)]
+
+        results = p.map_async(process_file, files)
+
+        while True:
+            if results.ready():
+                break
+            else:
+                size = q.qsize()
+                sys.stdout.write("on file: %i \r"%size)
+                time.sleep(0.3)
+                sys.stdout.flush()
+
+
+        output = results.get()
+
+        for row in output: 
+            try:
+                curr.execute("""INSERT INTO ArticleInformation 
+                                VALUES (?, ?, ?, ?)""", row)
+            except ValueError:
+                continue 
+            except sqlite3.IntegrityError:
+                continue
+
+        conn.commit()
