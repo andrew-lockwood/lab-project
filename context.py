b88e1514b7616f153dbaa971e7e206d22cfc004c
andrew-lockwood <andrewlockwood579@gmail.com>
2017-02-14 15:59:02 -0500

    added bow model to context

--- context.py
+++ context.py
@@ -5,3 +5,5 @@ sys.path.insert(0, os.path.abspath('..'))
 from util.settings import settings
 from util.progressbar import ProgressBar
 from corpus.data_loader import DataLoader
+from corpus.sentence_iterator import UnlabeledSentences
+from corpus.document_iterator import Documents
