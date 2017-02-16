b459753bafe7c20ac6aef850b050c8f33ac5ce1d
andrew-lockwood <andrewlockwood579@gmail.com>
2017-02-16 14:23:33 -0500

    settings file

---
+++ util/settings.py
@@ -0,0 +1,8 @@
+# Used for import directories into other modules.  Good for setting
+# project wide parameters, such as the location of the database. 
+
+class settings():
+	db = "C:\\Users\\Andrew\\lab_project\\database\\frontiers_corpus.db"
+	d2v300model = "C:\\Users\\Andrew\\lab_project\\models\\doc2vecmodels\\300model"
+	bowmodel = "C:\\Users\\Andrew\\lab_project\\models\\bagofwordsmodels\\bow.pickle"
+	xml = "C:\\Users\\Andrew\\Desktop\\frontiers_data\\article_xml"
