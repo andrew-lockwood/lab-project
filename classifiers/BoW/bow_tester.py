65a5baad49de7abce387c67915df0fab06e43615
andrew-lockwood <andrewlockwood579@gmail.com>
2017-02-22 14:17:51 -0500

    bow results (partial)

--- classifiers/BoW/bow_tester.py
+++ classifiers/BoW/bow_tester.py
@@ -1,14 +1,21 @@
-
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.cross_validation import KFold
 from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.cross_validation import KFold
 from sklearn.feature_extraction.text import CountVectorizer
-from time import time
-from context import DataLoader
-import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
-def run_bow_classifier(vectors, targets):
+import gensim.models
+
+from collections import defaultdict
+from time import time
+import numpy as np
+import csv
+
+from context import DataLoader
+
+
+
+def run_classifier(vectors, targets):
     k_fold = KFold(n=len(targets), n_folds=5)
     scores = []
     confusion = np.array([[0, 0], [0, 0]])
@@ -20,43 +27,76 @@ def run_bow_classifier(vectors, targets):
         test_vecs = [vectors[i] for i in test_indices] 
         test_y = [targets[i] for i in test_indices] 
 
-        gnb = MultinomialNB()
-        gnb.fit(train_vecs, train_y)
-        predictions = gnb.predict(test_vecs)
+        pipeline = Pipeline([   ('vectorizer',  CountVectorizer()),
+                                ('classifier',  MultinomialNB())    ])
+
+        pipeline.fit(train_vecs, train_y)
+        predictions = pipeline.predict(test_vecs)
         confusion += confusion_matrix(test_y, predictions)
         score = f1_score(test_y, predictions, pos_label='1')
         scores.append(score)
 
     avg_score = sum(scores)/len(scores)
 
-    return (len(vectors), avg_score, confusion)
+    return (avg_score, confusion)
+
+
+def summary_stats(x): 
+    """Takes an array and returns summary statistics."""
+    x = np.array(x)
+    return(np.mean(x), np.median(x), np.var(x))
+
 
 
 if __name__ == '__main__':
+    t0 = time()
+    data = DataLoader(70)
+    text_data = data.load_text()
 
-	print("Loading text with common keywords...")
-	t0 = time()
-	dl = DataLoader(70)
-	text = dl.load_text()
+    with open("bow_results.csv", "a") as f: 
+        fieldnames = ["keyword", "articles tested", 
+                        "score mean", "score median", "score var",
+                        "tp mean", "tp median", "tp var",
+                        "fp mean", "fp median", "fp var",
+                        "tn mean", "tn median", "tn var",
+                        "fn mean", "fn median", "fn var"]
+        w = csv.DictWriter(f, fieldnames=fieldnames)
 
-	print("Creating tf_model... ")
-	tf_model = CountVectorizer()
-	#tf_model.fit(text.values())
+        w.writeheader()
 
+        for kwd in data.get_keywords()[10:15]:
+            print(kwd)
+            scores = []
+            confusions = defaultdict(list)
 
-	print("Fitting test set")
-	dl.analyze("emotion")
+            for i in range(10):
+                data.analyze(kwd)
 
-	titles = dl.get_title_dict()
+                titles, targets = zip(*data.get_title_dict())
 
-	#vectors = tf_model.fit_transform([text[k] for k,v in titles])
+                text = [text_data[t] for t in titles]
+             
+                score, confusion = run_classifier(text, targets)
 
-	#targets = [v for k,v in titles]
+                scores.append(score)
 
-	vectors = []
+                confusions["true positive"].append(confusion[0][0])
+                confusions["false negative"].append(confusion[0][1])
+                confusions["false positive"].append(confusion[1][0])
+                confusions["true negative"].append(confusion[1][1])
 
-	gnb = MultinomialNB()
-    gnb.fit(train_vecs, train_y)
+            row = {}
 
-	run_bow_classifier(vectors, targets)
-	print("Time Elapsed: " + str(time()-t0))
+            row["keyword"] = kwd
+            row["articles tested"] = data.num_titles()
+
+            row["score mean"], row["score median"], row["score var"] = summary_stats(scores)
+
+            row["tp mean"], row["tp median"], row["tp var"] = summary_stats(confusions["true positive"])
+            row["fp mean"], row["fp median"], row["fp var"] = summary_stats(confusions["false positive"])
+            row["fn mean"], row["fn median"], row["fn var"] = summary_stats(confusions["false negative"])
+            row["tn mean"], row["tn median"], row["tn var"] = summary_stats(confusions["true negative"])
+
+            w.writerow(row)
+
+    print("Time Elapsed: " + str(time()-t0))
